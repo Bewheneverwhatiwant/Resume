09b12d1eca0b28e5e09712c4d538932b9cd5990b
nayeong lee <lny021102@gmail.com>
2024-09-04 00:31:54 +0900

    [ADD] 모든 프로젝트 추가

--- resume/src/app/mainpage/allprojects.tsx
+++ resume/src/app/mainpage/allprojects.tsx
@@ -5,18 +5,45 @@ import CustomButton from "@/components/CustomButton";
 import CustomColumn from "@/components/CustomColumn";
 import CustomFont from "@/components/CustomFont";
 import CustomRow from "@/components/CustomRow";
+import activities from "@/app/data/activities";
 
 export default function AllProjects() {
     return (
         <CustomColumn width='70%' alignitems="flex-start" justifycontent="center" gap='2rem'>
             <CustomFont color='white' font="2rem" fontweight="bold">모든 활동</CustomFont>
-            <CustomBox width='100%' height='30vh' overflowy="auto" alignitems="flex-start" justifycontent="flex-start" gap='0.5rem'>
-                <CustomFont color='white' font='1rem'>여기 모든 활동</CustomFont>
-                <CustomFont color='white' font='1rem'>여기 모든 활동</CustomFont>
-                <CustomFont color='white' font='1rem'>여기 모든 활동</CustomFont>
-                <CustomFont color='white' font='1rem'>여기 모든 활동</CustomFont>
-                <CustomFont color='white' font='1rem'>여기 모든 활동</CustomFont>
-                <CustomFont color='white' font='1rem'>여기 모든 활동</CustomFont>
+            <CustomBox width='100%' height='30vh' overflowy="auto" alignitems="flex-start" justifycontent="flex-start" gap='2rem'>
+                <CustomRow width='100%' alignitems="center" justifycontent="space-around">
+                    <CustomRow style={{ flex: 1 }}>
+                        <CustomFont color='white' font='1.2rem' fontweight='bold'>기관</CustomFont>
+                    </CustomRow>
+                    <CustomRow style={{ flex: 1 }}>
+                        <CustomFont color='white' font='1.2rem' fontweight='bold'>주관</CustomFont>
+                    </CustomRow>
+                    <CustomRow style={{ flex: 1 }}>
+                        <CustomFont color='white' font='1.2rem' fontweight='bold'>프로젝트명</CustomFont>
+                    </CustomRow>
+                    <CustomRow style={{ flex: 1 }}>
+                        <CustomFont color='white' font='1.2rem' fontweight='bold'>성과</CustomFont>
+                    </CustomRow>
+                </CustomRow>
+                <CustomColumn width='100%' gap='0.5rem' alignitems="center" justifycontent="center">
+                    {activities.map((activity, index) => (
+                        <CustomRow key={index} width='100%' alignitems="center" justifycontent="center" gap='1rem'>
+                            <CustomColumn style={{ flex: 1 }} justifycontent="center" alignitems="center">
+                                <CustomFont color='white' font='1rem'>{activity.기관}</CustomFont>
+                            </CustomColumn>
+                            <CustomColumn style={{ flex: 1 }} alignitems="flex-start">
+                                <CustomFont color='white' font='1rem'>{activity.주관}</CustomFont>
+                            </CustomColumn>
+                            <CustomColumn style={{ flex: 1 }} alignitems="flex-start">
+                                <CustomFont color='white' font='1rem'>{activity.프로젝트명}</CustomFont>
+                            </CustomColumn>
+                            <CustomColumn style={{ flex: 1 }} alignitems="flex-start">
+                                <CustomFont color='skyblue' font='1rem' fontweight='bold'>{activity.성과}</CustomFont>
+                            </CustomColumn>
+                        </CustomRow>
+                    ))}
+                </CustomColumn>
             </CustomBox>
         </CustomColumn>
     );
