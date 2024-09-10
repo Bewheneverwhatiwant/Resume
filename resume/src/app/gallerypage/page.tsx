1d9f8626bd790f0fa2798dd00c1308d10b698147
nayeong lee <lny021102@gmail.com>
2024-09-11 03:57:37 +0900

    [ADD] gallery 디자인 추가

--- resume/src/app/gallerypage/page.tsx
+++ resume/src/app/gallerypage/page.tsx
@@ -2,12 +2,15 @@
 
 import styled from "styled-components";
 import CustomFont from "@/components/CustomFont";
+import CustomColumn from "@/components/CustomColumn";
+import CustomRow from "@/components/CustomRow";
+import CustomDivider from "@/components/CustomDivider";
 
 const Container = styled.div`
 display: flex;
 flex-direction: column;
 align-items: center;
-justify-content: center;
+justify-content: flex-start;
 gap: 5rem;
 min-height: 100vh;
 padding: 1rem 10vh 10rem;
@@ -17,7 +20,15 @@ export default function GalleryPage() {
 
     return (
         <Container>
-            <CustomFont color='white' font='3rem'>여기는 갤러리</CustomFont>
+            <CustomColumn gap='1rem' width='100%'>
+                <CustomRow width='100%' justifycontent="flex-start">
+                    <CustomFont color='white' font='3rem'>Gallery.</CustomFont>
+                </CustomRow>
+                <CustomDivider backgroundcolor="white" width='100%' height='1px' />
+                <CustomRow width='100%' justifycontent="flex-end">
+                    <CustomFont color='white' font='1rem'>All the drawings were drawn by nayeong.</CustomFont>
+                </CustomRow>
+            </CustomColumn>
         </Container>
     );
 }
