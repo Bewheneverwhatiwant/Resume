201af6be4829b651475614483a3b41af314e7a63
nayeong lee <lny021102@gmail.com>
2024-09-11 13:16:47 +0900

    [ADD] gallery가 부드럽게 로딩되도록 효과 추가

--- resume/src/app/gallerypage/page.tsx
+++ resume/src/app/gallerypage/page.tsx
@@ -9,13 +9,13 @@ import CustomDivider from "@/components/CustomDivider";
 import StyledImg from "@/components/StyledImg";
 
 const Container = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: flex-start;
-gap: 5rem;
-min-height: 100vh;
-padding: 1rem 10vh 10rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 5rem;
+  min-height: 100vh;
+  padding: 1rem 10vh 10rem;
 `;
 
 interface ModalOverlayProps {
@@ -46,10 +46,23 @@ const ModalImage = styled.img<ModalOverlayProps>`
   transform: ${(props) => (props.isOpen ? "scale(1)" : "scale(0.8)")};
 `;
 
+const AnimatedStyledImg = styled(StyledImg)`
+  opacity: 0;
+  transform: scale(0.9);
+  transition: opacity 0.5s ease-in-out, transform 0.5s ease-in-out;
+
+  &.loaded {
+    opacity: 1 !important;
+    transform: scale(1) !important;
+  }
+`;
+
 export default function GalleryPage() {
     const [isModalOpen, setModalOpen] = useState(false);
+    const [modalImageSrc, setModalImageSrc] = useState("");
 
-    const handleImgClick = () => {
+    const handleImgClick = (src: string) => {
+        setModalImageSrc(src);
         setModalOpen(true);
     };
 
@@ -57,38 +70,42 @@ export default function GalleryPage() {
         setModalOpen(false);
     };
 
+    // 이미지 로드 이벤트 핸들러
+    const handleImageLoad = (event: React.SyntheticEvent<HTMLImageElement, Event>) => {
+        event.currentTarget.classList.add('loaded');
+    };
+
     return (
         <Container>
-            <CustomColumn gap='1rem' width='100%'>
-                <CustomRow width='100%' justifycontent="flex-start">
-                    <CustomFont color='white' font='3rem'>Gallery.</CustomFont>
+            <CustomColumn gap="1rem" width="100%">
+                <CustomRow width="100%" justifycontent="flex-start">
+                    <CustomFont color="white" font="3rem">Gallery.</CustomFont>
                 </CustomRow>
-                <CustomDivider backgroundcolor="white" width='100%' height='1px' />
-                <CustomRow width='100%' justifycontent="flex-end">
-                    <CustomColumn alignitems="flex-start" gap='0.5rem'>
-                        <CustomFont color='white' font='1rem'>All the drawings were drawn by nayeong.</CustomFont>
-                        <CustomFont color='white' font='1rem'>Sorted by newest!</CustomFont>
+                <CustomDivider backgroundcolor="white" width="100%" height="1px" />
+                <CustomRow width="100%" justifycontent="flex-end">
+                    <CustomColumn alignitems="flex-start" gap="0.5rem">
+                        <CustomFont color="white" font="1rem">All the drawings were drawn by nayeong.</CustomFont>
+                        <CustomFont color="white" font="1rem">Sorted by newest!</CustomFont>
                     </CustomColumn>
                 </CustomRow>
             </CustomColumn>
-            <CustomColumn width='100%'>
-                <CustomRow width='100%' justifycontent="space-between">
-                    <StyledImg src={'draw8.png'} width='25%' borderradius="0.5rem" onClick={handleImgClick} cursor='pointer' />
-                    <StyledImg src={'draw7.jpg'} width='25%' borderradius="0.5rem" onClick={handleImgClick} cursor='pointer' />
-                    <StyledImg src={'draw6.jpg'} width='25%' borderradius="0.5rem" onClick={handleImgClick} cursor='pointer' />
-                    <StyledImg src={'draw5.jpg'} width='25%' borderradius="0.5rem" onClick={handleImgClick} cursor='pointer' />
+            <CustomColumn width="100%">
+                <CustomRow width="100%" justifycontent="space-between">
+                    <AnimatedStyledImg src={'draw8.png'} width="25%" borderradius="0.5rem" onClick={() => handleImgClick('draw8.png')} onLoad={handleImageLoad} cursor="pointer" />
+                    <AnimatedStyledImg src={'draw7.jpg'} width="25%" borderradius="0.5rem" onClick={() => handleImgClick('draw7.jpg')} onLoad={handleImageLoad} cursor="pointer" />
+                    <AnimatedStyledImg src={'draw6.jpg'} width="25%" borderradius="0.5rem" onClick={() => handleImgClick('draw6.jpg')} onLoad={handleImageLoad} cursor="pointer" />
+                    <AnimatedStyledImg src={'draw5.jpg'} width="25%" borderradius="0.5rem" onClick={() => handleImgClick('draw5.jpg')} onLoad={handleImageLoad} cursor="pointer" />
                 </CustomRow>
-                <CustomRow width='100%' justifycontent="space-between">
-                    <StyledImg src={'draw4.jpg'} width='25%' borderradius="0.5rem" onClick={handleImgClick} cursor='pointer' />
-                    <StyledImg src={'draw3.jpg'} width='25%' borderradius="0.5rem" onClick={handleImgClick} cursor='pointer' />
-                    <StyledImg src={'draw2.jpg'} width='25%' borderradius="0.5rem" onClick={handleImgClick} cursor='pointer' />
-                    <StyledImg src={'draw1.jpg'} width='25%' borderradius="0.5rem" onClick={handleImgClick} cursor='pointer' />
+                <CustomRow width="100%" justifycontent="space-between">
+                    <AnimatedStyledImg src={'draw4.jpg'} width="25%" borderradius="0.5rem" onClick={() => handleImgClick('draw4.jpg')} onLoad={handleImageLoad} cursor="pointer" />
+                    <AnimatedStyledImg src={'draw3.jpg'} width="25%" borderradius="0.5rem" onClick={() => handleImgClick('draw3.jpg')} onLoad={handleImageLoad} cursor="pointer" />
+                    <AnimatedStyledImg src={'draw2.jpg'} width="25%" borderradius="0.5rem" onClick={() => handleImgClick('draw2.jpg')} onLoad={handleImageLoad} cursor="pointer" />
+                    <AnimatedStyledImg src={'draw1.jpg'} width="25%" borderradius="0.5rem" onClick={() => handleImgClick('draw1.jpg')} onLoad={handleImageLoad} cursor="pointer" />
                 </CustomRow>
             </CustomColumn>
 
             <ModalOverlay isOpen={isModalOpen} onClick={handleModalClose}>
-                {/* 여기 클릭하는 이미지에 따라 경로가 바뀌도록 수정하기 */}
-                <ModalImage src={"profileImg.jpg"} isOpen={isModalOpen} />
+                <ModalImage src={modalImageSrc} isOpen={isModalOpen} />
             </ModalOverlay>
         </Container>
     );
